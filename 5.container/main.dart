891b4009fa9a2af2ce0dae5a8a4a6e4a37db3f07
vohungson <45703910+vohungson@users.noreply.github.com>
2021-09-04 11:20:56 +0700

    Add files via upload

--- 5.container/main.dart
+++ 5.container/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 20, 30, 40), // padding is inside container
+        margin: EdgeInsets.all(30),  // margin is around container
+        color: Colors.grey[400],
+        child: Text('hello'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('click!'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
